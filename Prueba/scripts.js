a5d928e294d1f75907c6d980a2694e10f94bc290
Jorge Ruiz <97487049+jorgeruizm94@users.noreply.github.com>
2022-04-06 22:46:28 -0700

    Add files via upload

---
+++ Prueba/scripts.js
@@ -0,0 +1,28 @@
+const character = document.getElementById('character-box');
+        const block = document.getElementById('block');
+        document.addEventListener('click', jump);
+        function jump() {
+            if (character.classList == 'jump') {
+                return;
+            }
+            character.classList.add('jump');
+            setTimeout(() => {
+                character.classList.remove('jump');
+            }, 500);
+
+        }
+        function check() {
+
+            let blockleft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+            let characterbottom = parseInt(window.getComputedStyle(character).getPropertyValue('bottom'));
+            if (blockleft > 50 && blockleft < 300 && characterbottom <= 150) {
+                alert('Game Over');
+
+            }
+
+        }
+
+        setInterval(() => {
+            check();
+
+        }, 10);
