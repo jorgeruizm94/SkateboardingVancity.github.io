8b15540f9269a12718fbe7ebb30aca339c1b29a9
jorgeruizm94 <97487049+jorgeruizm94@users.noreply.github.com>
2022-04-07 18:20:13 -0700

    changes 2

--- Prueba/scripts.js
+++ Prueba/scripts.js
@@ -1,24 +1,66 @@
-const character = document.getElementById('character-box');
+const characterBox = document.getElementById('character-box');
         const block = document.getElementById('block');
+        const character = document.getElementById('character');
+
+
+
+        let gameover = new Audio('gameover.wav');
+        let music = new Audio('Battles.mp3');
+        let jumpMusic=new Audio('jump.wav');
+
+        let score = document.getElementById('score');
+        let scoreIncrease = 0;
+
+        music.loop = true;
+        music.play();
+        music.volume=0.5;
+
         document.addEventListener('click', jump);
         function jump() {
-            if (character.classList == 'jump') {
+            if (characterBox.classList == 'jump') {
                 return;
             }
-            character.classList.add('jump');
+            characterBox.classList.add('jump');
+            jumpMusic.play();
+           
             setTimeout(() => {
-                character.classList.remove('jump');
-            }, 500);
+                characterBox.classList.remove('jump');
+                jumpMusic.paused();
+                jumpMusic.currentTime=0;
+              
+              
+            }, 700);
 
         }
+
+        let flag = 1;
         function check() {
 
             let blockleft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
-            let characterbottom = parseInt(window.getComputedStyle(character).getPropertyValue('bottom'));
-            if (blockleft > 50 && blockleft < 300 && characterbottom <= 150) {
-                alert('Try Again!');
+            let characterbottom = parseInt(window.getComputedStyle(characterBox).getPropertyValue('bottom'));
+
+            if (blockleft > 30 && blockleft < 336 && characterbottom <= 150) {
+
+                music.pause();
+                block.style.animationPlayState = 'paused';
+                character.style.animationPlayState = 'paused';
+                characterBox.style.animationPlayState = 'paused';
+                jumpMusic.muted=true;
+
+
+                if (flag == 1) {
+                    gameover.play();
+                    flag++;
+                }
+
 
             }
+            else {
+
+                scoreIncrease++;
+                score.innerHTML = scoreIncrease;
+            }
+
 
         }
 
